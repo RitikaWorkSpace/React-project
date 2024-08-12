6cbba81b46b855d6daa4ae44c2146bb31e4b72c3
Ritika Raja Singh <ritika.s0099@yahoo.com>
2024-08-12 12:46:38 +0530

    link the cart page.

--- src/Component/Cart/Cart.jsx
+++ src/Component/Cart/Cart.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Cart = () => {
   return (
-    <div className='relative inline-block'>
+  
+   
+      <Link to='/CartPage'>
+       <div className='relative inline-block'>
       <img src="./Cart1.svg" className='ml-4 sm:ml-4' alt="Cart" />
       <div className='text-white bg-red-500 rounded-full w-6 h-6 flex items-center justify-center absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2'>
         0
       </div>
     </div>
+      </Link>
+     
   );
 };
 
