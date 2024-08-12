4dc044a91c32f8fc3fbdc6562ccca40d54443d37
Ritika Raja Singh <ritika.s0099@yahoo.com>
2024-08-12 13:25:33 +0530

    link ua page add3

--- src/Component/User Account/NavbarUser.jsx
+++ src/Component/User Account/NavbarUser.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from 'react-router-dom';
 
 
 const NavbarUser = () => {
@@ -20,38 +21,38 @@ const NavbarUser = () => {
 
       {/* Dropdown Menu */}
       {isDropdownOpen && (
-       <div className="absolute right-0 mt-2 hover:bg-[#DB4444] rounded w-[245px]">
-       <ul className="bg-gradient-to-r from-[#98729d] to-[#666267] text-white w-full py-[10px] px-[20px] list-none">
-       
-        <li className="flex items-center  gap-[16px] mb-[13px] cursor-pointer">
-          
-          <img src="./nac01user.svg" alt="navbar user icon" className="w-[20px] " />
-          <p>Manage My Account</p>
-          
-          
-         </li>
-       
-      
-         <li className="flex items-center  gap-[16px] mb-[13px] cursor-pointer">
-           <img src="./nac02Group.svg" alt="navbar user icon" />
-           <p>My Order</p>
-         </li>
-         <li className="flex items-center  gap-[16px] mb-[13px] cursor-pointer">
-           <img src="./nac03icon-cancel.svg" alt="navbar user icon" />
-            <p>My Cancellations</p>
-         </li>
-         <li className="flex items-center  gap-[16px] mb-[13px] cursor-pointer">
-           <img src="./nac04Icon-Reviews.svg" alt="navbar user icon" />
-          <p> My Reviews</p>
+        <div className="absolute right-0 mt-2 hover:bg-[#DB4444] rounded w-[245px]">
+          <ul className="bg-gradient-to-r from-[#98729d] to-[#666267] text-white w-full py-[10px] px-[20px] list-none">
 
-         </li>
-         <li className="flex items-center  gap-[16px] cursor-pointer">
-           <img src="./nac05Icon-logout.svg" alt="navbar user icon" />
-           <p>Logout</p>
-         </li>
-       </ul>
-     </div>
-     
+            <Link to='/UA'>
+              <li className="flex items-center  gap-[16px] mb-[13px] cursor-pointer">
+
+                <img src="./nac01user.svg" alt="navbar user icon" className="w-[20px] " />
+
+                <p>Manage My Account</p>
+              </li>
+            </Link>
+
+            <li className="flex items-center  gap-[16px] mb-[13px] cursor-pointer">
+              <img src="./nac02Group.svg" alt="navbar user icon" />
+              <p>My Order</p>
+            </li>
+            <li className="flex items-center  gap-[16px] mb-[13px] cursor-pointer">
+              <img src="./nac03icon-cancel.svg" alt="navbar user icon" />
+              <p>My Cancellations</p>
+            </li>
+            <li className="flex items-center  gap-[16px] mb-[13px] cursor-pointer">
+              <img src="./nac04Icon-Reviews.svg" alt="navbar user icon" />
+              <p> My Reviews</p>
+
+            </li>
+            <li className="flex items-center  gap-[16px] cursor-pointer">
+              <img src="./nac05Icon-logout.svg" alt="navbar user icon" />
+              <p>Logout</p>
+            </li>
+          </ul>
+        </div>
+
       )}
     </div>
   );
